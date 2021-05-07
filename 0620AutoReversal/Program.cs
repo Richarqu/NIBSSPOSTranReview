7f60bf8a4f3c32599f08a2c4207ffb93192f4b64
Richarqu <richarque@gmail.com>
2021-05-07 12:13:03 +0100

    First commit, application for reviewing NIBSS POS transaction failures for auto reversal Application is similar to 0620 Camguard auto reversal.

---
+++ 0620AutoReversal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIBSSPOSTranReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logics _logics = new Logics();
+            //get POS Data within the hour check
+            var _getFailedPosTran = _logics.GetDailyPOSData();
+            //populate data into a list in preparation for CamGuard validation
+            var _fillDataList = _logics.FillDataList(_getFailedPosTran);
+            //validate each record in the list against Office and take decision
+            _logics.CheckPostilionOffice(_fillDataList);
+            //once record validation is complete, generate a .csv of records not found on postilion to be shared with Interswitch.
+        }
+    }
+}
