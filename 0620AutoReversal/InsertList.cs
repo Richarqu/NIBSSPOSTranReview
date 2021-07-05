943b3aec2d6bbb2686b9aed92efaac6a53c812b6
Richarqu <richarque@gmail.com>
2021-07-05 17:44:33 +0100

    FEP connection completed, Postcard connection added, and insertion into database for processing methods completed.

---
+++ 0620AutoReversal/InsertList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0620AutoReversal
+{
+    public class InsertList
+    {
+        //,Original_FT_Debit_Acct,Remark,Original_FT_Credit_Acct,Original_FT_Ref
+    }
+}
